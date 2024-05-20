537d38b12faa07e0463895b421bf83a429f33eea
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-20 07:02:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here
 
+let cnt=0;
+
+let mDiv = document.getElementById("app");
+let btn = document.getElementById("swap");
+
+function swapTheme(){
+    cnt++;
+    if(cnt%2==0){
+        mDiv.setAttribute("class","day-ev");
+        btn.setAttribute("class","button_day_ev");
+        document.body.style.backgroundColor = "white";
+    }
+    else{
+        mDiv.setAttribute("class","day-od");
+        btn.setAttribute("class","button_day_od");
+        document.body.style.backgroundColor = "black";
+    }
+}
